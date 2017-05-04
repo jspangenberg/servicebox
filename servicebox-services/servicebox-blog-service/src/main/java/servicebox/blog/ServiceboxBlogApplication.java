735a5fcf29887eeb864ef9bb5abfa1442e63aec5
JOHN SPANGENBERG <ja.spangenberg@gmail.com>
2017-05-04 19:38:39 +0200

    Restructured project setup.

---
+++ servicebox-services/servicebox-blog-service/src/main/java/servicebox/blog/ServiceboxBlogApplication.java
@@ -0,0 +1,17 @@
+package servicebox.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author John Spangenberg
+ */
+
+@SpringBootApplication
+public class ServiceboxBlogApplication {
+
+    public static void main(String args) {
+        SpringApplication.run(ServiceboxBlogApplication.class, args);
+    }
+
+}
