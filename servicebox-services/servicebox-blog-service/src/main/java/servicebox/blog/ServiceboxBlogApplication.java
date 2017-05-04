3eecb2a123cb5a3eb7202ce07893e4f92ec6f378
JOHN SPANGENBERG <ja.spangenberg@gmail.com>
2017-05-04 19:47:47 +0200

    Added SpringBoot main class

--- servicebox-services/servicebox-blog-service/src/main/java/servicebox/blog/ServiceboxBlogApplication.java
+++ servicebox-services/servicebox-blog-service/src/main/java/servicebox/blog/ServiceboxBlogApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceboxBlogApplication {
 
-    public static void main(String args) {
+    public static void main(String[] args) {
         SpringApplication.run(ServiceboxBlogApplication.class, args);
     }
 
